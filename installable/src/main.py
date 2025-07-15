860108191ab236f846d88fdae6757687d0a98ca3
Tarikul69 <tarikulabir69@gmail.com>
2025-07-15 21:16:24 +0600

    Update

--- installable/src/main.py
+++ installable/src/main.py
@@ -13,19 +13,4 @@ setup(
         ],
     },
 )
-########################################################
-from setuptools import setup, find_packages
 
-setup(
-    name="my_project",
-    version="0.1",
-    packages=find_packages(),
-    install_requires=[
-        # list your dependencies
-    ],
-    entry_points={
-        'console_scripts': [
-            'mytool = src.main:main',
-        ],
-    },
-)
